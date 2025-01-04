18dfdf5ea19323ceeceb75e1a3af597d7939e4d3
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2025-01-04 12:11:03 +0100

    basic e2e

--- tests/E2E/Connections.php
+++ tests/E2E/Connections.php
@@ -11,11 +11,11 @@ class Connections
         return [
             'MySql' => (object)[
                 'driver' => new MySqlDriver(),
-                'connection' => new \PDO('mysql:host=mysql;dbname=test_db', 'root', 'root')
+                'connection' => new \PDO('mysql:host=localhost;dbname=test_db', 'root', 'root')
             ],
             'SqlServer'=> (object)[
                 'driver' => new \Mkrawczyk\DbQueryTranslator\Driver\SqlServer\SqlServerDriver(),
-                'connection' => new \PDO('sqlsrv:Server=sqlserver;Database=test_db', 'sa', 'RootRoot1')
+                'connection' => new \PDO('sqlsrv:Server=localhost;Database=test_db', 'sa', 'RootRoot1')
             ]
         ];
     }
