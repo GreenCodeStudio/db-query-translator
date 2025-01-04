4fa514850dd8fc41785c398d71d6886a641a83e6
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2025-01-04 15:07:08 +0100

    mognodb

--- src/Driver/MongoDB/MongoDBDriver.php
+++ src/Driver/MongoDB/MongoDBDriver.php
@@ -1,7 +1,14 @@
 <?php
 
 namespace Mkrawczyk\DbQueryTranslator\Driver\MongoDB;
+
+use Mkrawczyk\DbQueryTranslator\Driver\MongoDB\Serializer\MongoDBSerializer;
+
 class MongoDBDriver
 {
-
+    public function serialize($node)
+    {
+        $serializer = new MongoDBSerializer();
+        return $serializer->serialize($node);
+    }
 }
