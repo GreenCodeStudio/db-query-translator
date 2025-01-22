ea22253ee959c0ce7f0c9f7db0567ab85336333b
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2025-01-22 23:31:53 +0100

    bugfix

--- src/Nodes/Expression/Identifier.php
+++ src/Nodes/Expression/Identifier.php
@@ -6,7 +6,7 @@ class Identifier
     public string $name;
     public ?string $table = null;
 
-    public function __construct(string $name, string $table = null)
+    public function __construct(string $name, ?string $table = null)
     {
         $this->name = $name;
         $this->table = $table;
